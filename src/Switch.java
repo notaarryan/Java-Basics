53a45c4f9b49a779c87eb1d37f607de37a4df450
notaarryan <aryanparmar2503@gmail.com>
2024-08-02 16:17:43 +0530

    switch

--- src/Switch.java
+++ src/Switch.java
@@ -1,3 +1,6 @@
+//after each case in switch remember to break using break; or else all the statements will be executed
+import java.util.*;
+
 public class Switch {
     static String getSeason (int monthNumber) {
         String season = "";
@@ -5,40 +8,61 @@ public class Switch {
             case 1:
                 System.out.println("Season is Winter");
                 season = "Winter";
+                break;
             case 2:
                 System.out.println("Season is Winter");
                 season = "Winter";
+                break;
             case 3:
                 System.out.println("Season is Winter");
                 season = "Winter";
+                break;
             case 4:
                 System.out.println("Season is Winter");
                 season = "Winter";
+                break;
             case 5:
                 System.out.println("Season is Summer");
                 season = "Summer";
+                break;
             case 6:
                 System.out.println("Season is Summer");
                 season = "Summer";
+                break;
             case 7:
                 System.out.println("Season is Summer");
                 season = "Summer";
+                break;
             case 8:
                 System.out.println("Season is Summer");
                 season = "Summer";
+                break;
             case 9:
                 System.out.println("Season is Spring");
                 season = "Spring";
+                break;
             case 10:
                 System.out.println("Season is Spring");
                 season = "Spring";
+                break;
             case 11:
                 System.out.println("Season is Spring");
                 season = "Spring";
+                break;
             case 12:
                 System.out.println("Season is Spring");
                 season = "Spring";
+                break;
         }
         return season;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter month number: ");
+        int monthNumber = sc.nextInt();
+        System.out.println();
+        String season = getSeason(monthNumber);
+        System.out.println(season);
     }
 }
