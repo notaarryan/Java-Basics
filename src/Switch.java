ead1ea858a0cc7dff3e13f0822a2823d8ccb2abb
notaarryan <aryanparmar2503@gmail.com>
2024-08-02 16:08:23 +0530

    new class

---
+++ src/Switch.java
@@ -0,0 +1,44 @@
+public class Switch {
+    static String getSeason (int monthNumber) {
+        String season = "";
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Season is Winter");
+                season = "Winter";
+            case 2:
+                System.out.println("Season is Winter");
+                season = "Winter";
+            case 3:
+                System.out.println("Season is Winter");
+                season = "Winter";
+            case 4:
+                System.out.println("Season is Winter");
+                season = "Winter";
+            case 5:
+                System.out.println("Season is Summer");
+                season = "Summer";
+            case 6:
+                System.out.println("Season is Summer");
+                season = "Summer";
+            case 7:
+                System.out.println("Season is Summer");
+                season = "Summer";
+            case 8:
+                System.out.println("Season is Summer");
+                season = "Summer";
+            case 9:
+                System.out.println("Season is Spring");
+                season = "Spring";
+            case 10:
+                System.out.println("Season is Spring");
+                season = "Spring";
+            case 11:
+                System.out.println("Season is Spring");
+                season = "Spring";
+            case 12:
+                System.out.println("Season is Spring");
+                season = "Spring";
+        }
+        return season;
+    }
+}
