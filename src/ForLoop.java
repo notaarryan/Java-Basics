3bfad52776f17e67cbddbf56a6e7e9ff29dc05a7
notaarryan <aryanparmar2503@gmail.com>
2024-08-05 00:36:48 +0530

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        int[] array = {0,1,2,3,4,5,6,7,8,9};
+        for (int i = 0 ; i<array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
