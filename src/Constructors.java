3f7023788bce93719ddca069c27d01d8095c349c
notaarryan <aryanparmar2503@gmail.com>
2024-08-10 17:31:33 +0530

    Constructors class updated

--- src/Constructors.java
+++ src/Constructors.java
@@ -15,15 +15,15 @@ public class Constructors {
     double tutionFee = 12000.0;
     double internationalFee = 5000.0;
 
-    Constructors(int newid, String newName, String newGender, int newAge, long newPhone, double newGpa, char newDegree, boolean isInternational){
-        id = newid;
-        name = newName;
-        gender = newGender;
-        age = newAge;
-        phone = newPhone;
-        gpa = newGpa;
-        degree = newDegree;
-        international = isInternational;
+    Constructors(int id, String name, String gender, int age, long phone, double gpa, char degree, boolean international){
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.phone = phone;
+        this.gpa = gpa;
+        this.degree = degree;
+        this.international = international;
     }
 
     void computeFee() {
