46107195e0db180db5a214d37f561040750689ed
notaarryan <aryanparmar2503@gmail.com>
2024-08-14 18:00:53 +0530

    FinalVariables.java added

---
+++ src/FinalVariables.java
@@ -0,0 +1,13 @@
+//a variable declared final implies that its value is constant
+//for primitives ~ value is constant
+//for reference variables ~ reference is constant
+//final variables do not get default values
+
+//Final Instance Variable
+//constant for the life of the object
+//must be initialized in:
+//1.declaration statement
+//2.
+
+public class FinalVariables {
+}
